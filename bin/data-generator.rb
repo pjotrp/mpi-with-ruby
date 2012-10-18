aef6ad70fb297181534cd845c25d6c550d375fcd
Pjotr Prins <pjotr.public01@thebird.nl>
2012-10-18 13:41:59 +0200

    Data generator

---
+++ bin/data-generator.rb
@@ -0,0 +1,45 @@
+# Generate input file
+
+SIZE=10_000          
+HAPLOTYPES = 4 
+MUTATION_RATE = 200  # 1 in 200
+NUCLEOTIDES = "a g c t".split
+
+individuals = ARGV[0].to_i
+
+reference = []
+
+(0..SIZE-1).each do | i |
+  reference <<  [NUCLEOTIDES[rand(4)],NUCLEOTIDES[rand(4)]]
+end
+
+# basically the reference is a list of known SNPs
+# We simulate haplotypes by combining 'known SNPs'
+
+haplotypes = []
+(0..HAPLOTYPES-1).each do | h |
+  haplotypes[h] = []
+  (0..SIZE-1).each do | i |
+    if rand(MUTATION_RATE) == 1
+      haplotypes[h] << 'x'
+    else
+      haplotypes[h] << reference[i][rand(2)]
+    end
+  end
+end
+
+(1..individuals).each do | i |
+  fn = "ind#{i}.tab"
+  print "\nWriting #{fn}..."
+  File.open(fn,"w") do | f |
+    h = 0
+    (0..SIZE-1).each do | i |
+      if i % 300 == 0   
+        h = rand(HAPLOTYPES)
+      end
+      prob = rand * 2
+      prob = 1.0 if prob > 1
+      f.printf "%s\t%s\t%.2f\n",reference[i],haplotypes[h][i],prob
+    end
+  end
+end
---
+++ bin/reader.rb
@@ -0,0 +1,5 @@
+# Reader reads data from a file and stores strong SNPs in shared space (Tokyocabinet)
+
+ARGF.each do | line |
+  p line
+end
