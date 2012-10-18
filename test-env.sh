e8587de02f3583cbdadfc3ff6885090cdb63a71c
Pjotr Prins <pjotr.public01@thebird.nl>
2012-10-18 12:15:23 +0200

    Test script

---
+++ test-env.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+np=$1
+if [ -z $np ]; then np=4 ; fi
+
+time env LD_LIBRARY_PATH=~/.rvm/rubies/ruby-1.8.7-p352/lib/ mpiexec -np $np contrib/mpi-ruby/src/mpi_ruby example/basic-test.rb 
+time env LD_LIBRARY_PATH=~/.rvm/rubies/ruby-1.8.7-p352/lib/ mpiexec -np $np contrib/mpi-ruby/src/mpi_ruby -I /home/wrk/.rvm/gems/ruby-1.8.7-p352/gems/tokyocabinet-1.29/ example/tokyocabinet-test.rb 
+
