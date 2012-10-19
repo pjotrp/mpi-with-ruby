9537f8b2163f6a02a17b2f1942c96fb357522cd5
Pjotr Prins <pjotr.public01@thebird.nl>
2012-10-19 18:17:24 +0200

    Test script

--- test-env.sh
+++ test-env.sh
@@ -7,4 +7,4 @@ rvm use 1.8.7
 time env LD_LIBRARY_PATH=~/.rvm/rubies/ruby-1.8.7-p352/lib/ mpiexec -np $np contrib/mpi-ruby/src/mpi_ruby example/basic-test.rb 
 time env LD_LIBRARY_PATH=~/.rvm/rubies/ruby-1.8.7-p352/lib/ mpiexec -np $np contrib/mpi-ruby/src/mpi_ruby -I /home/wrk/.rvm/gems/ruby-1.8.7-p352/gems/tokyocabinet-1.29/ example/tokyocabinet-test.rb 
 
-ruby bin/data-generator.rb 16
+# ruby bin/data-generator.rb 16
