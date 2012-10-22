748b63a06be985b7bbe019e6e9e9bc7f56f22bd6
Pjotr Prins <pjotr.public01@thebird.nl>
2012-10-22 21:59:23 +0200

    The splitter is identical across runs, so the problem is later

--- bin/haplotype-responder.rb
+++ bin/haplotype-responder.rb
@@ -3,7 +3,7 @@ $: << './lib'
 require "parseline"
 
 VERBOSE = false
-DO_SPLIT = false      # Read input file in split fashion
+DO_SPLIT = true      # Read input file in split fashion
 PROB_THRESHOLD = 0.5
 MPI_ANY_SOURCE = -1  # from /usr/lib/openmpi/include/mpi.h
 MPI_ANY_TAG    = -1  # from /usr/lib/openmpi/include/mpi.h
@@ -67,7 +67,7 @@ def handle_responder pid,f,individual,individuals
     if $snp_cache.size ==0 or end_pos > $snp_cache.last.pos 
       # continue filling the cache, until we have reached the right section
       ParseLine::tail_each_genotype(f) do | g |
-        # puts "["+g.to_s+"]"
+        puts "["+g.to_s+"]"
         $snp_cache << g
         if DO_SPLIT
           break if g.pos >= end_pos
