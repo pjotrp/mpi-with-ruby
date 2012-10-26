d9f4b87d4408b3e5421f669e784868f84489728d
Pjotr Prins <pjotr.public01@thebird.nl>
2012-10-26 11:38:34 +0200

    Updated script

--- test-lisa.sh
+++ test-lisa.sh
@@ -15,7 +15,7 @@ module load openmpi/gnu
 
 # rvm use 1.8.7
 ruby=/home/pjotrp/opt/ruby/mpi-ruby/src/mpi_ruby
-time env LD_LIBRARY_PATH=/home/pjotrp/.rvm/rubies/ruby-1.8.7-p371/lib mpiexec -np $NUM $ruby -I $HOME/.rvm/gems/ruby-1.8.7-p371/gems/json-1.7.5/lib/ bin/snp-scorer.rb : -np $NUM $ruby -I $HOME/.rvm/gems/ruby-1.8.7-p371/gems/json-1.7.5/lib/ bin/haplotype-responder.rb
+time env LD_LIBRARY_PATH=/home/pjotrp/.rvm/rubies/ruby-1.8.7-p371/lib mpiexec -np $NUM $ruby -I $HOME/.rvm/gems/ruby-1.8.7-p371/gems/json-1.7.5/lib/ bin/snp-scorer.rb  ./etc/pid2filename.txt : -np $NUM $ruby -I $HOME/.rvm/gems/ruby-1.8.7-p371/gems/json-1.7.5/lib/ bin/haplotype-responder.rb  ./etc/pid2filename.txt
 
 mkdir -p test/regression/data
 cp snp[1234].tab test/regression/data
