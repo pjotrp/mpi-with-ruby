97902e181910c497f17c1130e7495e3f038bb306
Pjotr Prins <pjotr.public01@thebird.nl>
2012-10-26 15:18:04 +0200

    Sleep notice

--- lib/filereader.rb
+++ lib/filereader.rb
@@ -15,8 +15,9 @@ module FileReader
       else
         # We are moving too fast
         # $stderr.print "reader pause"
-        break if sleep_counter > 180*100 # break out after 3 minutes
+        break if sleep_counter > 60*100 # break out after 60 seconds
         sleep_counter += 1
+        $stderr.print "W" if sleep_counter % 600 == 0
         sleep 0.01
         select([f]) #  <-- use when producer is slower than reader
       end
